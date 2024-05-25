86ccd2c91728ac4f635051336b91c47b89ea811a
Tyler Young <tyleryoung88@yahoo.com>
2024-05-25 17:36:57 -0400

    Initial NZP QuakeC upload. No functionality yet other than spawning into a server.

---
+++ tools/qc-compiler-win.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+CD ../
+
+REM ****** generate hash table ******
+echo Generating Hash Table..
+python3 bin\qc_hash_generator.py -i tools\asset_conversion_table.csv -o source\server\hash_table.qc
+
+REM ****** create build directories ******
+MKDIR build\fte\ 2>nul
+MKDIR build\standard\ 2>nul
+
+CD bin/
+
+REM ****** build.. ******
+echo Compiling FTE CSQC..
+fteqcc-cli-win.exe -srcfile ../progs/fte-client.src
+echo Compiling FTE SSQC..
+fteqcc-cli-win.exe -O2 -srcfile ../progs/fte-server.src
+echo Compiling Standard/Id SSQC..
+fteqcc-cli-win.exe -O2 -progdefs -srcfile ../source/standard.src
+
+echo End of script.
+pause
